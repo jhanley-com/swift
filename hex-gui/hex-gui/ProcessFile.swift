ca8d5e972a8b7346ea1cdd4fbf8ee32bad9995b0
John Hanley <john.hanley@jhanley.com>
2022-10-04 19:41:27 -0700

    Update copyright in source files

--- hex-gui/hex-gui/ProcessFile.swift
+++ hex-gui/hex-gui/ProcessFile.swift
@@ -1,3 +1,17 @@
+/*****************************************************************************
+* Date Created: 2022-10-04
+* Last Update:  2022-10-04
+* https://www.jhanley.com
+* Copyright (c) 2020, John J. Hanley
+* Author: John J. Hanley
+* License: MIT
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*****************************************************************************/
+
 import Foundation
 
 /*
